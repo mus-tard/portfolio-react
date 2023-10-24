ff8a9d4872e1b5c89d9e45e490b002b72e3fe5de
Charles K. Delahunt <120161030+mus-tard@users.noreply.github.com>
2023-10-24 13:41:15 -0400

    Fix netlify build error

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useSiteMetadata } from "../hooks/use-site-metadata"
 
-export const Seo = ({ title, description, pathname, children }) => {
+function Seo({ title, description, pathname, children }) {
     const { 
         title: defaultTitle, 
         description: defaultDescription, image, siteUrl 
@@ -28,3 +28,5 @@ export const Seo = ({ title, description, pathname, children }) => {
       </>
     )
   }
+
+  export default Seo
