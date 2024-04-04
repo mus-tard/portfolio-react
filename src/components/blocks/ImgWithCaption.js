ec52b45e18871e4ce8ea32f7bc2b5da8464163ab
Charles K. Delahunt <120161030+mus-tard@users.noreply.github.com>
2024-04-04 08:25:34 -0400

    Add ability to remove drop shadow

--- src/components/blocks/ImgWithCaption.js
+++ src/components/blocks/ImgWithCaption.js
@@ -3,25 +3,27 @@ import { getImage, GatsbyImage } from 'gatsby-plugin-image';
 
 import {
     image,
+    imageNoShadow,
     content,
     container,
     figcaption
 } from './imgWithCaption.module.css'
 
 
-function ImgWithCaption({caption, data, imgName}) {
+function ImgWithCaption({caption, data, imgName, dropShadow}) {
 
-    const imageData = data.find(img => imgName === img.name)
+    const imageData = data.find(img => imgName === img.name);
 
-    const imgSource = getImage(imageData.childCloudinaryAsset)
+    const imgSource = getImage(imageData.childCloudinaryAsset);
 
-    const alt = imageData.childCloudinaryAsset.rawCloudinaryData.context.custom.alt
+    const alt = imageData.childCloudinaryAsset.rawCloudinaryData.context.custom.alt;
     
+    const imageStyle = dropShadow ? image : imageNoShadow;
 
     return(
         <figure className={container}>
             <div className={content}>
-                <GatsbyImage className={image} image={imgSource} alt={alt}/>
+                <GatsbyImage className={imageStyle} image={imgSource} alt={alt}/>
                 <figcaption className={figcaption}>{caption}</figcaption>
             </div>
         </figure>
